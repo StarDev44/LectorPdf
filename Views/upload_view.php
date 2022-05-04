750b32b44ae60c04aeccfa11cda37a411d6ecabc
Dylan Estrella <dylanestrella2@gmail.com>
2022-05-04 17:27:36 -0500

    Upload View Modified

--- Views/upload_view.php
+++ Views/upload_view.php
@@ -1,8 +1,12 @@
 <main class="mx-3 text-center">
+    <form action="">
+
+    </form>
     <div class="p-5 mt-3 col-12 col-lg-6 border border-3  border-dark">
         <p class="card-text">
-            
+
         </p>
+        <input class="form-control my-5" type="file" id="formFile">
         <a id="upload" href="/PDF_LECTOR/Pages/View.php" class="btn btn-dark fw-bold mt-0 mt-lg-5 px-5 py-2">
             Upload PDF
         </a> 
